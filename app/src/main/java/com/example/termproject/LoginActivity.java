49a69bc85a7de52c69a7ba07d0af50e690cd9f0b
Eunho Bae <fere1032@gmail.com>
2022-11-28 02:33:49 +0900

    로그인 시 realtime db에서 학교이름 가져온 후 sharedpref에 저장

--- app/src/main/java/com/example/termproject/LoginActivity.java
+++ app/src/main/java/com/example/termproject/LoginActivity.java
@@ -14,10 +14,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance("https://termproject-3711d-default-rtdb.asia-southeast1.firebasedatabase.app/");
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         auth = FirebaseAuth.getInstance();
         TextView tv_email = findViewById(R.id.tv_email);
         TextView tv_password  = findViewById(R.id.tv_password);
-
+        databaseReference = database.getReference();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +70,20 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(getApplicationContext(), "로그인 성공!", Toast.LENGTH_LONG).show();
+                            FirebaseUser currentUser = auth.getCurrentUser();
+                            String uid = currentUser.getUid();
+                            databaseReference.child("Users").child(uid).child("Univ").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    Toast.makeText(getApplicationContext(), snapshot.getValue(String.class), Toast.LENGTH_LONG).show();
+                                    PrefManager.setString(getApplicationContext(), "Univ", snapshot.getValue(String.class));
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
 
                             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                             startActivity(intent);
